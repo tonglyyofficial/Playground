f86da6987839c10508540457cbb67ced2b69128d
tonglyyofficial <natitida.sa@gmail.com>
2023-12-04 23:44:43 +0700

    updated script in main

--- booking-app/main.go
+++ booking-app/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"booking-app/helper"
 	"fmt"
-	"strings"
+	"sync"
+	"time"
 )
 
 const conferenceTickets int = 50 //can not := using with const
 var conferenceName = "Go Conference"
-var remainingTickets uint = 50
-var bookings = []string{}
+var RemainingTickets uint = 50
+
+// var bookings = make([]map[string]string, 0)
+var bookings = make([]UserData, 0)
+
+type UserData struct {
+	firstName       string
+	lastName        string
+	email           string
+	numberOfTickets uint
+}
+
+var wg = sync.WaitGroup{}
 
 func main() {
 
@@ -18,81 +31,58 @@ func main() {
 	//Go --> int8,int16,int32,int64 | uInt: Positive, whole numbers
 	//Java --> byte,short,int,long
 
-	for {
+	firstName, lastName, email, userTickets := getUserInput()
+	fmt.Println(firstName)
+	isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(firstName, lastName, email, userTickets, RemainingTickets)
 
-		firstName, lastName, email, userTickets := getUserInput()
-		fmt.Println(firstName)
-		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets)
+	if isValidName && isValidEmail && isValidTicketNumber {
+		// fmt.Printf("the whole slice: %v\n", bookings)
+		// fmt.Printf("the first value: %v\n", bookings[0])
+		// fmt.Printf("slice type: %T\n", bookings)
+		// fmt.Printf("slice length: %v\n", len(bookings))
 
-		if isValidName && isValidEmail && isValidTicketNumber {
-			// fmt.Printf("the whole slice: %v\n", bookings)
-			// fmt.Printf("the first value: %v\n", bookings[0])
-			// fmt.Printf("slice type: %T\n", bookings)
-			// fmt.Printf("slice length: %v\n", len(bookings))
+		bookTicket(userTickets, firstName, lastName, email)
 
-			bookTicket(userTickets, firstName, lastName, email)
+		wg.Add(1)
+		go sendTicket(userTickets, firstName, lastName, email)
 
-			FirstNames := getFirstNames()
-			fmt.Printf("The first names of bookings are: %v\n", FirstNames)
+		FirstNames := getFirstNames()
+		fmt.Printf("The first names of bookings are: %v\n", FirstNames)
 
-			if remainingTickets == 0 {
-				// end program
-				fmt.Println("Our conference is booked out. Come back next year")
-				break
-			}
+		if RemainingTickets == 0 {
+			// end program
+			fmt.Println("Our conference is booked out. Come back next year")
+			// break
+		}
 
-		} else {
-			if !isValidName {
-				fmt.Println("firstname or lastname you entered is too short")
-			}
-			if !isValidEmail {
-				fmt.Println("email address you entered doesn't contain @ sign")
-			}
-			if !isValidTicketNumber {
-				fmt.Println("number of tickets you entered is invalid")
-			}
-
+	} else {
+		if !isValidName {
+			fmt.Println("firstname or lastname you entered is too short")
+		}
+		if !isValidEmail {
+			fmt.Println("email address you entered doesn't contain @ sign")
+		}
+		if !isValidTicketNumber {
+			fmt.Println("number of tickets you entered is invalid")
 		}
 
 	}
-
-	// city := "London"
-
-	// switch city{
-	// 	case "New York":
-	// 		// execute code for booking New York conference tickets
-	// 	case "Singapore","Hong Kong":
-	// 		// execute code for booking Singapore conference tickets
-	// 	case "London","Berlin":
-	// 		// execute code for booking London conference tickets
-	// 	case "Mexico city":
-	// 		// execute code for booking Mexico city conference tickets
-	// 	default:
-	// 		fmt.Println("No valid city selected")
-	// }
-
+	wg.Wait()
 }
 
 func greetUsers() {
 	fmt.Printf("Welcome to our %v booking application\n", conferenceName)
-	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, RemainingTickets)
 	fmt.Println("Get your tickets here to attend")
 }
 
 func getFirstNames() []string {
 	firstNames := []string{}
 	for _, booking := range bookings {
-		var names = strings.Fields(booking)
-		firstNames = append(firstNames, names[0])
+		// firstNames = append(firstNames, booking["firstName"])
+		firstNames = append(firstNames, booking.firstName)
 	}
 	return firstNames
-}
-
-func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
-	isValidName := len(firstName) >= 2 && len(lastName) >= 2
-	isValidEmail := strings.Contains(email, "@")
-	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
-	return isValidName, isValidEmail, isValidTicketNumber
 }
 
 func getUserInput() (string, string, string, uint) {
@@ -117,8 +107,52 @@ func getUserInput() (string, string, string, uint) {
 }
 
 func bookTicket(userTickets uint, firstName string, lastName string, email string) {
-	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, firstName+" "+lastName)
+	RemainingTickets = RemainingTickets - userTickets
+
+	// create a map for a user
+	// var userData = make(map[string]string)
+	var userData = UserData{
+		firstName:       firstName,
+		lastName:        lastName,
+		email:           email,
+		numberOfTickets: userTickets,
+	}
+	// userData["firstName"] = firstName
+	// userData["lastName"] = lastName
+	// userData["email"] = email
+	// userData["numberOfTickets"] = strconv.FormatUint(uint64(userTickets), 10)
+
+	bookings = append(bookings, userData)
+	fmt.Printf("List of bookings is %v\n", bookings)
+
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickest remaining for %v\n", remainingTickets, conferenceName)
+	fmt.Printf("%v tickest remaining for %v\n", RemainingTickets, conferenceName)
 }
+
+func sendTicket(userTickets uint, firstName string, lastName string, email string) {
+	time.Sleep(50 * time.Second)
+	var ticket = fmt.Sprintf("%v tickets for %v %v", userTickets, firstName, lastName)
+	fmt.Println("###########################")
+	fmt.Printf("Sending ticket:\n %v \nto email address %v\n", ticket, email)
+	fmt.Println("###########################")
+	wg.Done()
+}
+
+/// 3 Levels of Scope
+///Local--> Declaration within function or block (can be used only within that function or block)
+///Package --> Declaration outside all function (can be used everywhere in the same package)
+/// Global --> Declarartion outside all function & uppercase first letter (cna be used everywhere across all packages)
+
+// city := "London"
+// switch city{
+// 	case "New York":
+// 		// execute code for booking New York conference tickets
+// 	case "Singapore","Hong Kong":
+// 		// execute code for booking Singapore conference tickets
+// 	case "London","Berlin":
+// 		// execute code for booking London conference tickets
+// 	case "Mexico city":
+// 		// execute code for booking Mexico city conference tickets
+// 	default:
+// 		fmt.Println("No valid city selected")
+// }
